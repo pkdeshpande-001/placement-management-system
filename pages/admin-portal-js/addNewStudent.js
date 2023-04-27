edfcb5ce12235d5619873109bce4878fc22538e7
Prathmesh kalburgi <prathameshkalburgi@gmail.com>
2023-04-27 16:22:58 +0530

    added exp leter

--- pages/admin-portal-js/addNewStudent.js
+++ pages/admin-portal-js/addNewStudent.js
@@ -6,39 +6,73 @@ function saveAddCustomer(){
   var inputOfferLetter = document.getElementById("inputOfferLetter").files[0];
   var inputOnOffCampus = document.getElementById("inputOnOffCampus").value;
   var inutSelectPassoutYear = document.getElementById("inutSelectPassoutYear").value;
+  var inputExprienceLetter = document.getElementById("inputExprienceLetter").files[0];
+
+  var offerName = inputUsn+"-"+inputCompany+"-OfferLetter"
+  var expName = inputUsn+"-"+inputCompany+"-exprienceLetter"
+
+    const promises = [];
+        promises.push(uploadImageAsPromise(inputOfferLetter,offerName))
+        promises.push(uploadImageAsPromise(inputExprienceLetter,expName))
+    
+    //The Promise.all() will stop the execution, until all of the promises are resolved.
+    Promise.all(promises).then((fileURLS)=>{
+        //Once all the promises are resolved, you will get the urls in a array.
+        console.log(fileURLS)
+        addToFileBase( inputUsn, inputStudentName, inputCompany, inputOnOffCampus, inutSelectPassoutYear,fileURLS);
+    })
   
+
+
+}
+const storageRef = firebase.storage().ref('StudentDoc');
+
+  //Upload Image Function returns a promise  
+  async function uploadImageAsPromise(imageFile,name) {
+    return new Promise(function (resolve, reject) {
+      const task = storageRef.child(name).put(imageFile);
+
+      task.on(
+        "state_changed",
+        function progress(snapshot) {
+          const percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        },
+
+        function error(err) {
+          reject(err);
+        },
+
+        async function complete() {
+          //The getDownloadURL returns a promise and it is resolved to get the image url.
+          const imageURL = await task.snapshot.ref.getDownloadURL();
+          resolve(imageURL);
+        }
+      );
+    });
+  }
   
-  // console.log(key)
+  //Handling the files
+  
 
-  var offerUrl;
-  const ref=firebase.storage().ref('StudentDoc');
-    const name=inputCompany+"-"+inputUsn;
-    const metadata={
-        contentType:document.getElementById("inputOfferLetter").files[0].type
-    };
 
-    const task=ref.child(name).put(inputOfferLetter,metadata);
-    task
-    .then(snapshot => snapshot.ref.getDownloadURL())
-    .then (url =>{
-     
-        console.log(url);
-        offerUrl=url;
-        var key = firebase.database().ref().child('PlacementStudent/').push().key;
-        firebase.database().ref('PlacementStudent/'+key).set({
-          usn: inputUsn,
-          studentName: inputStudentName,
-          key:key,
-          company:inputCompany,
-          offer: offerUrl,
-          campus: inputOnOffCampus,
-          passoutYear: inutSelectPassoutYear
-          
-        }).then(
-                  alert("Successfully added"),
-                  document.getElementById("spinner").style.display = "none"
-          );
-          
-});
-
+function addToFileBase( inputUsn, inputStudentName, inputCompany, inputOnOffCampus, inutSelectPassoutYear,fileURLS) {
+  console.log(fileURLS[0]);
+  console.log(fileURLS[1]);
+  var offerUrl = fileURLS[0];
+  var expUrl = fileURLS[1];
+  var key = firebase.database().ref().child('PlacementStudent/').push().key;
+  firebase.database().ref('PlacementStudent/' + key).set({
+    usn: inputUsn,
+    studentName: inputStudentName,
+    key: key,
+    company: inputCompany,
+    offer: offerUrl,
+    exp: expUrl,
+    campus: inputOnOffCampus,
+    passoutYear: inutSelectPassoutYear
+  }).then(
+    alert("Successfully added"),
+    document.getElementById("spinner").style.display = "none"
+  );
+  return offerUrl;
 }
