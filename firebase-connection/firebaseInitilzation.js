1a7bebf6ef92dc29578970a8b126b3723578822b
snehasidnal <102715904+snehasidnal@users.noreply.github.com>
2023-05-10 10:40:40 +0530

    Update firebaseInitilzation.js
    changed the configuration of database storage and auth

--- firebase-connection/firebaseInitilzation.js
+++ firebase-connection/firebaseInitilzation.js
@@ -1,13 +1,13 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyAz9oETBwcUjab28EF8g94I9KJ8-FnD_g8",
-  authDomain: "student-lms-5740d.firebaseapp.com",
-  databaseURL: "https://student-lms-5740d-default-rtdb.firebaseio.com",
-  projectId: "student-lms-5740d",
-  storageBucket: "student-lms-5740d.appspot.com",
-  messagingSenderId: "687228699331",
-  appId: "1:687228699331:web:f23771ea54e78605601876",
-  measurementId: "G-L92DSSLHK2"
-};
+ const firebaseConfig = {
+    apiKey: "AIzaSyDk3N-OQI4WcHf28LlwzQu2_RGDNVe0jxQ",
+    authDomain: "student-placement-fa18a.firebaseapp.com",
+    databaseURL: "https://student-placement-fa18a-default-rtdb.firebaseio.com",
+    projectId: "student-placement-fa18a",
+    storageBucket: "student-placement-fa18a.appspot.com",
+    messagingSenderId: "263544537761",
+    appId: "1:263544537761:web:6bb85086162a0617a226e0",
+    measurementId: "G-K9DXTCK65L"
+  };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
   firebase.analytics();
